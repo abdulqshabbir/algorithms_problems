f3d7f1333ec6750fe10716ed94ba7bb69e95c468
Abdul Shabbir <abdulqshabbir@gmail.com>
2020-10-19 07:55:45 +0800

    optimizedEqualSumPartition complete.

--- Arrays/equalSumPartition.test.ts
+++ Arrays/equalSumPartition.test.ts
@@ -1,5 +1,13 @@
-import { equalSumPartition } from './equalSumPartition'
+import { equalSumPartition, optimizedEqualSumPartition } from './equalSumPartition'
 
 test("equalSumPartition works", () => {
     expect(equalSumPartition([-1, 6, 3, 1, -2, 3, 3])).toStrictEqual(2)
+})
+
+test("optimizedEqualSumPartition works", () => {
+    expect(equalSumPartition([-1, 6, 3, 1, -2, 3, 3])).toStrictEqual(2)
+})
+
+test("optimizedEqualSumPartition works for 3 element arary", () => {
+    expect(equalSumPartition([1, 0, 1])).toStrictEqual(1)
 })
--- Arrays/equalSumPartition.ts
+++ Arrays/equalSumPartition.ts
@@ -1,4 +1,30 @@
-/* Given an array of integers, find an index that
+/* OPTIMIZECD SOLUTION: Given an array of integers, find an index that
+divides the array into two non-empty subarrays having
+equal sum */
+
+/* Preprocess the array by finding the sum of all elements. Then, for
+each partitionIndex (i) from 1..N-1, slowly build-up the leftSum
+by adding A[i] each time and find the rightSum by using the formula
+rightSum = totalSum - leftSum - A[i]. If leftSum === rightSum, return i.*/
+
+
+export const optimizedEqualSumPartition = (arr: number[]): number | null => {
+    let totalSum = 0
+    for (let i = 0; i < arr.length; i++) {
+        totalSum += arr[i]
+    }
+
+    let leftSum = arr[0]
+    let rightSum
+
+    for (let pIndex = 1; pIndex < arr.length - 1; pIndex++) {
+        rightSum = totalSum - leftSum - arr[pIndex]
+        if (leftSum === rightSum) return pIndex
+        leftSum += arr[pIndex]
+    }
+    return null
+}
+/* NAIVE SOLUTION: Given an array of integers, find an index that
 divides the array into two non-empty subarrays having
 equal sum */
 
