f47082141d5cfee811a5ba0a844cfc5dd788bfdd
Abdul Shabbir <abdulqshabbir@gmail.com>
2020-11-01 09:45:18 +0800

    re-wrote the code for isMinHeap to account for when an internal node has no right child.

--- Heaps/isMinHeap.ts
+++ Heaps/isMinHeap.ts
@@ -6,20 +6,24 @@
 export const isMinHeap = (arr: number[], i: number = 0): boolean => {
     debugger;
     // base case: leaf nodes satisfy min-heap property
-    if (i >= Math.floor((arr.length - 2) / 2)) {
+    if (i > Math.floor((arr.length - 2) / 2)) {
         return true
     }
-    // recursive step: check if root >= each child and if left and right
+    // recursive step: check if root <= each child and if left and right
     // subtrees also are min-heaps
-    if (
+
+    let leftSubtreeIsMinHeap =
         arr[i] <= arr[2 * i + 1] &&
-        arr[i] <= arr[2 * i + 2] &&
-        isMinHeap(arr, 2 * i + 1) &&
-        isMinHeap(arr, 2 * i + 2)
-    ) {
-        return true
-    }
-    return false
+        isMinHeap(arr, 2 * i + 1)
+
+    let hasNoRightChild = 2 * i + 2 >= arr.length
+
+    let rightSubtreeIsMinHeap =
+        (hasNoRightChild) ||
+        (arr[i] <= arr[2 * i + 2] &&
+            isMinHeap(arr, 2 * i + 2))
+
+    return leftSubtreeIsMinHeap && rightSubtreeIsMinHeap
 }
 
 export const isMinHeapIterative = (arr: number[]): boolean => {
--- index.ts
+++ index.ts
@@ -1,11 +1,3 @@
-import { MinHeap } from './Heaps/MinHeap'
+import { isMinHeap } from './Heaps/isMinHeap'
 
-let nodes = []
-let keys = [1, 12, 3, 5, 10]
-
-for (let key of keys) {
-    nodes.push({ key: key, value: key })
-}
-
-let heap = new MinHeap(nodes)
-heap.minHeapify(1)
+isMinHeap([2, 3, 4, 5, 10, 15])
